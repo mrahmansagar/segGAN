d8fc8c98ee3f78a53e2e42370a347f6df4adb223
mrahmansagar <mrahman.sagar@gmail.com>
2024-11-13 11:42:53 +0100

    modified ploting

--- data_gen_validation.py
+++ data_gen_validation.py
@@ -16,65 +16,95 @@ from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import img_as_ubyte
+from scipy import ndimage as nd
+
 
 from GANs import utils
 from keras.models import load_model
 
 
-val_dir = "E:\\Data\\segGAN\\val\\"
+val_dir = "E:\\Data\\segGAN\\ground_truth\\val\\"
 
 patch_dir = os.path.join(val_dir, "images")
 mask_dir = os.path.join(val_dir, "masks")
 
-masks = utils.load_images_in_shape(mask_dir, color_mode = 'grayscale')
-patches = utils.load_images_in_shape(patch_dir, color_mode = 'grayscale')
+all_masks = utils.load_images_in_shape(mask_dir, color_mode = 'grayscale')
+all_patches = utils.load_images_in_shape(patch_dir, color_mode = 'grayscale')
 
-masks = (masks > 0).astype('float32')
+all_masks = (all_masks > 0).astype('float32')
 
 
-plt.imshow(masks[0], cmap='gray')
+plt.imshow(all_masks[0], cmap='gray')
 plt.show()
-
-
-masks = (masks - 127.5) / 127.5
-
 
 model_dir = 'E:\\projects\\segGAN\\mask2patch_202411061638\\'
 
 all_trained_model = glob(model_dir + '*.h5')
 
-
 model_path = all_trained_model[99]
 
 model = load_model(model_path)
 
-gen_patch = model.predict(masks)
 
+
+sample_size = 5
+idx = np.random.randint(0, len(all_masks), sample_size)
+
+patches = all_patches[idx]
+
+masks = all_masks[idx]
+masks = (masks - 127.5) / 127.5
+
+gen_patch = model.predict(masks)
 gen_patch = (gen_patch + 1) / 2.0 
 
-gen_patch = img_as_ubyte(np.squeeze(gen_patch[0]))
+# Apply Median filter 
+# gen_patch = nd.median_filter(gen_patch, size=2)
+# Apply Gaussian blur
+# sigma = 0.5  # Standard deviation for Gaussian kernel
+# gen_patch = nd.gaussian_filter(gen_patch, sigma=sigma)
 
 
-plt.imshow(gen_patch, cmap='gray')
+# #gen_patch = img_as_ubyte(gen_patch)
+
+
+# ploting the images 
+plt.figure(figsize=(sample_size*2, sample_size*1.5))
+plt.tight_layout()
+plt.subplots_adjust(wspace=0, hspace=0)
+for i in range(sample_size):
+    plt.subplot(3, sample_size, i+1)
+    plt.axis('off')
+    plt.imshow(masks[i], cmap='gray')
+    
+    plt.subplot(3, sample_size, sample_size+1+i)
+    plt.axis('off')
+    #apply median filter 
+    # plt.imshow(nd.median_filter(gen_patch[i], size=2), cmap='gray')
+    plt.imshow(gen_patch[i], cmap='gray')
+
+    # Display generated patches in the third row
+    plt.subplot(3, sample_size, 2 * sample_size + i + 1)
+    plt.axis('off')
+    plt.imshow(patches[i], cmap='gray')
+    
+    
 plt.show()
 
 
-plt.imshow(patches[0], cmap='gray')
-plt.show()
+
+# from scipy import ndimage as nd
+# gen_patch_median = nd.median_filter(gen_patch, size=3)
 
 
-from scipy import ndimage as nd
-gen_patch_median = nd.median_filter(gen_patch, size=3)
+# plt.imshow(gen_patch_median, cmap='gray')
+# plt.show()
 
 
-plt.imshow(gen_patch_median, cmap='gray')
-plt.show()
+# # Step 4: Apply Gaussian blur
+# sigma = 0.5  # Standard deviation for Gaussian kernel
+# gen_patch_gaussian = nd.gaussian_filter(gen_patch, sigma=sigma)
 
-
-# Step 4: Apply Gaussian blur
-sigma = 0.5  # Standard deviation for Gaussian kernel
-gen_patch_gaussian = nd.gaussian_filter(gen_patch, sigma=sigma)
-
-plt.imshow(gen_patch_gaussian, cmap='gray')
-plt.show()
+# plt.imshow(gen_patch_gaussian, cmap='gray')
+# plt.show()
 
